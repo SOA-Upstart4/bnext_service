605c6937b6aa813986294fdd180d8c480801896e
HW-Lee <tony123930@yahoo.com.tw>
2015-10-25 21:25:09 +0800

    Add error msg notification system

--- model/bnext_feeds.rb
+++ model/bnext_feeds.rb
@@ -15,11 +15,21 @@ class RankList
   end
 
   def to_json
-    @rank.each do |feed_hash|
-      keys = feed_hash.keys.map { |k| k.dup.force_encoding(Encoding::UTF_8) }
-      values = feed_hash.values.map { |v| if v.class == String then v.force_encoding(Encoding::UTF_8) else v end }
-      Hash[keys.zip(values)]
-    end.to_json
+    if @rank.class == Array
+      @rank.each do |feed_hash|
+        keys = feed_hash.keys.map { |k| k.dup.force_encoding(Encoding::UTF_8) }
+        values = feed_hash.values.map { |v| if v.class == String then v.force_encoding(Encoding::UTF_8) else v end }
+        Hash[keys.zip(values)]
+      end.to_json
+    else
+      error_msg = "[Bad request] please check the category and the page no is rational\n\n\n"
+      error_msg += "Page no   : should be a natural number, a.k.a. POSITIVE INTEGER, and cannot be too large.\n"
+      error_msg += "Categories:\n"
+      @rank.each do |k, v|
+        error_msg += "\t\"#{v}\", for searching \"#{k}\"\n"
+      end
+      error_msg
+    end
   end
 end
 
@@ -49,7 +59,10 @@ class RankFeeds
     when 'feed'
       feed_found = bnext_robot.get_feeds(@category, @page)
       if feed_found.length == 0
-        puts 'Error: No result found. Check the input or internet connection.'
+        # puts 'Error: No result found. Check the input or internet connection.'
+        cat_titles = bnext_robot.cats.keys.map { |k| k.dup.force_encoding(Encoding::UTF_8) }
+        cat_urls = bnext_robot.cats.values.map { |v| v.dup.force_encoding(Encoding::UTF_8).split('/')[-1] }
+        @rank = RankList.new(Hash[cat_titles.zip(cat_urls)])
       else
         @rank = RankList.new(feed_found.map(&:to_hash))
       end
