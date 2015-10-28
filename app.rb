d8dc2009b6c9897fe82bef7aac3871ceadff030c
ichbinangela <angela.hung@iss.nthu.edu.tw>
2015-10-28 12:14:27 +0800

    restructure app.rb

--- app.rb
+++ app.rb
@@ -2,15 +2,20 @@ $KCODE = 'u' if RUBY_VERSION < '1.9'
 
 require 'sinatra/base'
 require_relative './model/bnext_feeds'
+require_relative 'bnext_helper'
 
+##
+# Simple web service to crawl Bnext webpages
 class BNextcadetApp < Sinatra::Base
   helpers BNextHelpers
 
-  get '/' do
-    'This is version 1. Our Github homepage is : https://github.com/SOA-Upstart4/bnext_service'
+  get_root = lambda do
+    'This is version 0.0.1. See documentation at its ' \
+      '<a href="https://github.com/SOA-Upstart4/bnext_service">' \
+      'Github repo</a>'
   end
-
-  get '/api/v1/:ranktype' do
+  
+  get_feed_ranktype = lambda do
     content_type :json, 'charset' => 'utf-8'
     cat = 'tech'
     page_no = 1
@@ -20,4 +25,10 @@ class BNextcadetApp < Sinatra::Base
     get_ranks(params[:ranktype], cat, page_no).to_json
   end
   
+  # to do: POST to be added here
+
+  # Web API Routes
+  get '/', &get_root
+  get '/api/v1/:ranktype', &get_feed_ranktype
+  # to do: POST route to be added here
 end
