a1aa80e973fefedd47e36ff3e33a77e2562d5074
ichbinangela <angela.hung@iss.nthu.edu.tw>
2015-11-07 22:29:06 +0800

    Seperate helper file

--- helpers/bnext_helper.rb
+++ helpers/bnext_helper.rb
@@ -6,13 +6,4 @@ module BNextHelpers
   rescue
     halt 404
   end
-
-  def newest_feeds(categories)
-    categories.map do |category|
-      found = RankFeeds.fetch('feed', category, '1')
-      [category, found[0]]
-    end.to_h
-  rescue
-    halt 404
-  end
 end
---
+++ helpers/trend_helper.rb
@@ -0,0 +1,12 @@
+##
+# Helpers for main Sinatra web application
+module BNextHelpers
+  def newest_feeds(categories)
+    categories.map do |category|
+      found = RankFeeds.fetch('feed', category, '1')
+      [category, found[0]]
+    end.to_h
+  rescue
+    halt 404
+  end
+end
