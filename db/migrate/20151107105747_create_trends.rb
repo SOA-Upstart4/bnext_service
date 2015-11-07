5d48d908595c0f9e57c7b4ef8558b0ad03a627c2
ichbinangela <angela.hung@iss.nthu.edu.tw>
2015-11-07 20:31:01 +0800

    Add new column "categories" in db

--- db/migrate/20151107105747_create_trends.rb
+++ db/migrate/20151107105747_create_trends.rb
@@ -4,6 +4,7 @@ class CreateTrends < ActiveRecord::Migration
       t.string :description
       t.string :links # link of the feed itself
       t.string :titles
+      t.string :categories
       t.string :authors
       t.string :dates # edited date of the feeed
       t.text :contents
