4bb2867039e5a7ca3b7997332359a5ec8c4c8e22
ichbinangela <angela.hung@iss.nthu.edu.tw>
2015-11-07 20:09:34 +0800

    Create model for migration

---
+++ db/migrate/20151107105747_create_trends.rb
@@ -0,0 +1,16 @@
+class CreateTrends < ActiveRecord::Migration
+  def change
+    create_table :trends do |t|
+      t.string :description
+      t.string :links # link of the feed itself
+      t.string :titles
+      t.string :authors
+      t.string :dates # edited date of the feeed
+      t.text :contents
+      t.string :tags
+      t.string :imgs
+
+      t.timestamps null: false
+    end
+  end
+end
