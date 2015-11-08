b7a430a0c17318ade0aa40556453784eed1aef2e
LeeTung-Yu <dfg1021@hotmail.com.tw>
2015-11-08 15:48:47 +0800

    update test for post route

--- spec/trend_spec.rb
+++ spec/trend_spec.rb
@@ -3,13 +3,34 @@ require_relative 'spec_answers'
 require 'json'
 
 describe 'Checking newest feeds' do
+  before do
+    Trend.delete_all
+  end
+
   it 'should return tech & marketing feeds' do
     header = { 'CONTENT_TYPE' => 'application/json' }
-    body = { categories: ['tech', 'marketing'] }
+    body = {
+      description: 'Check tech and marketing feeds',
+      categories: %w(tech marketing) }
+
+    # Check redirect URL from post request
+    post '/api/v1/recent', body.to_json, header
+    last_response.must_be :redirect?
+    next_location = last_response.location
+    next_location.must_match %r{api\/v1\/trend\/\d+}
+
+    # Check if request parameters are stored in ActiveRecord data store
+    trend_id = next_location.scan(%r{trend\/(\d+)}).flatten[0].to_i
+    save_trend = Trend.find(trend_id)
+    JSON.parse(save_trend[:categories]).must_equal body[:categories]
+
+    # Check if redirect works
     VCR.use_cassette('post_recent') do
-      post '/api/v1/recent', body.to_json, header
+      follow_redirect!
     end
-    last_response.must_be :ok?
+    last_request.url.must_match %r{api\/v1\/trend\/\d+}
+
+    # Check if redirected response has results
     last_response.body.must_equal(Answer::RECENT)
   end
 
@@ -22,9 +43,13 @@ describe 'Checking newest feeds' do
 
   it 'should return 404 for unknown category type' do
     header = { 'CONTENT_TYPE' => 'application/json' }
-    body = { categories: [random_str(10)] }
+    body = {
+      description: 'Check invalid categories type',
+      categories: [random_str(10)] }
+    post '/api/v1/recent', body.to_json, header
+    last_response.must_be :redirect?
     VCR.use_cassette('post_random') do
-      post '/api/v1/recent', body.to_json, header
+      follow_redirect!
     end
     last_response.body.must_match(/null/)
   end
